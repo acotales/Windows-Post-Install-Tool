14f46e551789a10a9e5f27f00179df2f5f18b637
Anthony Cotales <ton.cotales@gmail.com>
2026-02-04 09:27:59 +0800

    chore: add script lifecycle management utilities
    - Add mutex logic to ensure single script instance
    - Introduce Exit-Console function for clean session termination

--- WindowsPostInstallTool.ps1
+++ WindowsPostInstallTool.ps1
@@ -13,12 +13,43 @@
 #>
 
 using namespace System.Security.Principal
+using namespace System.IO
 
-$Principal = [WindowsPrincipal][WindowsIdentity]::GetCurrent()
+function Exit-Console {
+    try {
+        $script:Mutex.ReleaseMutex()
+        $script:Mutex.Dispose()
+    } catch { }
+    # Check raw command line arguments
+    $commandLine = [System.Environment]::CommandLine
+    if ($commandLine -like "*-NoExit*") {
+        Stop-Process -Id $PID -Force
+    } else { exit }
+}
+
+$CurrentPrincipal = [WindowsPrincipal][WindowsIdentity]::GetCurrent()
 $AdminRole = [WindowsBuiltInRole]::Administrator
+$MutexName = "Global\" + [Path]::GetFileNameWithoutExtension($PSCommandPath)
+
+$script:Mutex = New-Object System.Threading.Mutex($false, $MutexName)
 
 # -- CLEANER SELF-ELEVATION --
-if (-not $Principal.IsInRole($AdminRole)) {
+if (-not $CurrentPrincipal.IsInRole($AdminRole)) {
+    $ElevationArgs = @{
+        FilePath     = "powershell.exe"
+        Verb         = "RunAs"
+        ArgumentList = "-NoProfile -ExecutionPolicy Bypass -NoExit -File `"$PSCommandPath`""
+    }
+    Start-Process @ElevationArgs
     exit
 }
 
+# -- ENSURE SINGLE INSTANCE --
+if (-not $script:Mutex.WaitOne(0, $false)) { Exit-Console }
+
+Register-EngineEvent PowerShell.Exiting -Action {
+    try {
+        $script:Mutex.ReleaseMutex()
+        $script:Mutex.Dispose()
+    } catch { }
+} | Out-Null
